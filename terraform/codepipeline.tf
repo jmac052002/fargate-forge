813dd80f3731def3df45ecd5bb5a9a9a35cf0619
Joseph McCoy <jmac052002@gmail.com>
2026-04-09 00:11:22 -0500

    fix: upgrade pipeline to V2 type for CodeDeploy ECS blue/green

--- terraform/codepipeline.tf
+++ terraform/codepipeline.tf
@@ -127,7 +127,7 @@ resource "aws_iam_role_policy" "codepipeline" {
 resource "aws_codepipeline" "app" {
   name     = "${var.project_name}-pipeline"
   role_arn = aws_iam_role.codepipeline.arn
-
+  pipeline_type = "V2"
   artifact_store {
     location = aws_s3_bucket.artifacts.bucket
     type     = "S3"
@@ -182,12 +182,12 @@ resource "aws_codepipeline" "app" {
       version         = "1"
       input_artifacts = ["build_output"]
 
-      configuration = {
-        ApplicationName     = aws_codedeploy_app.app.name
-        DeploymentGroupName = aws_codedeploy_deployment_group.app.deployment_group_name
-      }
+        configuration = {
+          ApplicationName     = aws_codedeploy_app.app.name
+          DeploymentGroupName = aws_codedeploy_deployment_group.app.deployment_group_name
     }
   }
+}  
 
   tags = {
     Name = "${var.project_name}-pipeline"
